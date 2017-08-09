dec722228433c2abe4cf54f23662d3c3423db5cd
yopcyuuu <yopcyuuu@localhost>
2017-08-09 01:04:21 +0000

    test

---
+++ PS3.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "mbed.h"
+
+enum CONTROLLER {
+    UP, RIGHT, DOWN, LEFT,
+    TRIANGLE, CIRCLE, CROSS, SQUARE,
+    L1, R1,
+    SELECT, START,
+    L3, R3,
+    CONNECTION,
+    LeftHatX, LeftHatY,
+    RightHatX, RightHatY,
+    L2, R2
+};
+
+class PS3
+{
+private:
+    char data[8];
+    int DATA_CONTROLLER_OLD[21];
+    I2C * i2c;
+    char ADDR;
+public:
+    int DATA_CONTROLLER[21];
+    PS3(I2C *i, char addr);
+    bool UpDate();
+    int getPress( CONTROLLER buttom );
+    int getClick( CONTROLLER button, bool mode );
+};
