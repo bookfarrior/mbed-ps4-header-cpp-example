ef7fbfe092ec60c54c2fae52adfb9ff210dbb94e
yopcyuuu <yopcyuuu@localhost>
2017-09-08 13:14:41 +0000

    fix controller data when i2c error

--- PS3.cpp
+++ PS3.cpp
@@ -17,35 +17,36 @@ bool PS3::UpDate()
     memcpy( DATA_CONTROLLER_OLD, DATA_CONTROLLER, sizeof(DATA_CONTROLLER) );
 
     //Get data by I2C
-    i2c->read((ADDR | 1), data, 8);
+    if(i2c->read((ADDR | 1), data, 8) == 0) {
 
-    //UpDate
-    DATA_CONTROLLER[CONNECTION] = (data[1] >> 1) & 0x01;
-    DATA_CONTROLLER[UP] = (data[0] >> 7) & 0x01;
-    DATA_CONTROLLER[RIGHT] = (data[0] >> 6) & 0x01;
-    DATA_CONTROLLER[DOWN] = (data[0] >> 5) & 0x01;
-    DATA_CONTROLLER[LEFT] = (data[0] >> 4) & 0x01;
-    DATA_CONTROLLER[TRIANGLE] = (data[0] >> 3) & 0x01;
-    DATA_CONTROLLER[CIRCLE] = (data[0] >> 2) & 0x01;
-    DATA_CONTROLLER[CROSS] = (data[0] >> 1) & 0x01;
-    DATA_CONTROLLER[SQUARE] = (data[0] >> 0) & 0x01;
-    DATA_CONTROLLER[L1] = (data[1] >> 7) & 0x01;
-    DATA_CONTROLLER[R1] = (data[1] >> 6) & 0x01;
-    DATA_CONTROLLER[SELECT] = (data[1] >> 5) & 0x01;
-    DATA_CONTROLLER[START] = (data[1] >> 4) & 0x01;
-    DATA_CONTROLLER[L3] = (data[1] >> 3) & 0x01;
-    DATA_CONTROLLER[R3] = (data[1] >> 2) & 0x01;
-    DATA_CONTROLLER[LeftHatX] = data[2];
-    DATA_CONTROLLER[LeftHatY] = data[3];
-    DATA_CONTROLLER[RightHatX] = data[4];
-    DATA_CONTROLLER[RightHatY] = data[5];
-    DATA_CONTROLLER[L2] = data[6];
-    DATA_CONTROLLER[R2] = data[7];
-    if(DATA_CONTROLLER[CONNECTION] == 1) {
-        return 1;
+        //UpDate
+        DATA_CONTROLLER[CONNECTION] = (data[1] >> 1) & 0x01;
+        DATA_CONTROLLER[UP] = (data[0] >> 7) & 0x01;
+        DATA_CONTROLLER[RIGHT] = (data[0] >> 6) & 0x01;
+        DATA_CONTROLLER[DOWN] = (data[0] >> 5) & 0x01;
+        DATA_CONTROLLER[LEFT] = (data[0] >> 4) & 0x01;
+        DATA_CONTROLLER[TRIANGLE] = (data[0] >> 3) & 0x01;
+        DATA_CONTROLLER[CIRCLE] = (data[0] >> 2) & 0x01;
+        DATA_CONTROLLER[CROSS] = (data[0] >> 1) & 0x01;
+        DATA_CONTROLLER[SQUARE] = (data[0] >> 0) & 0x01;
+        DATA_CONTROLLER[L1] = (data[1] >> 7) & 0x01;
+        DATA_CONTROLLER[R1] = (data[1] >> 6) & 0x01;
+        DATA_CONTROLLER[SELECT] = (data[1] >> 5) & 0x01;
+        DATA_CONTROLLER[START] = (data[1] >> 4) & 0x01;
+        DATA_CONTROLLER[L3] = (data[1] >> 3) & 0x01;
+        DATA_CONTROLLER[R3] = (data[1] >> 2) & 0x01;
+        DATA_CONTROLLER[LeftHatX] = data[2];
+        DATA_CONTROLLER[LeftHatY] = data[3];
+        DATA_CONTROLLER[RightHatX] = data[4];
+        DATA_CONTROLLER[RightHatY] = data[5];
+        DATA_CONTROLLER[L2] = data[6];
+        DATA_CONTROLLER[R2] = data[7];
     } else {
-        return 0;
+        for(int i = 0; i < 21; i++){
+            DATA_CONTROLLER[i] = i >= 15 && i <= 18 ? 127 : 0;
+        }
     }
+    return DATA_CONTROLLER[CONNECTION];
 }
 
 int PS3::getPress( CONTROLLER buttom )
