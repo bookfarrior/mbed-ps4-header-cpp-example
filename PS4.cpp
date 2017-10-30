cff057609e80cfb23242fd54530fb4114cabdc8b
Suzutomo <Suzutomo@localhost>
2017-10-30 09:03:49 +0000

    final version

--- PS4.cpp
+++ PS4.cpp
@@ -5,7 +5,7 @@ PS4::PS4(I2C *i, char addr)
     i2c = i;
     ADDR = addr;
 
-    for(int i=0; i<21; i++) {
+    for(int i=0; i<23; i++) {
         DATA_CONTROLLER[i] = 0;
         DATA_CONTROLLER_OLD[i] = 0;
     }
@@ -17,34 +17,41 @@ bool PS4::UpDate()
     memcpy( DATA_CONTROLLER_OLD, DATA_CONTROLLER, sizeof(DATA_CONTROLLER) );
 
     //Get data by I2C
-    int s = i2c->read((ADDR | 1), data, 8);
+    int s = i2c->read((ADDR | 1), data, 17);
 
     if(s == 0) {
 
         //UpDate
-        DATA_CONTROLLER[CONNECTION] = (data[1] >> 1) & 0x01;
-        DATA_CONTROLLER[UP] = (data[0] >> 7) & 0x01;
-        DATA_CONTROLLER[RIGHT] = (data[0] >> 6) & 0x01;
-        DATA_CONTROLLER[DOWN] = (data[0] >> 5) & 0x01;
-        DATA_CONTROLLER[LEFT] = (data[0] >> 4) & 0x01;
-        DATA_CONTROLLER[TRIANGLE] = (data[0] >> 3) & 0x01;
-        DATA_CONTROLLER[CIRCLE] = (data[0] >> 2) & 0x01;
-        DATA_CONTROLLER[CROSS] = (data[0] >> 1) & 0x01;
-        DATA_CONTROLLER[SQUARE] = (data[0] >> 0) & 0x01;
-        DATA_CONTROLLER[L1] = (data[1] >> 7) & 0x01;
-        DATA_CONTROLLER[R1] = (data[1] >> 6) & 0x01;
-        DATA_CONTROLLER[SHARE] = (data[1] >> 5) & 0x01;
-        DATA_CONTROLLER[OPTIONS] = (data[1] >> 4) & 0x01;
-        DATA_CONTROLLER[L3] = (data[1] >> 3) & 0x01;
-        DATA_CONTROLLER[R3] = (data[1] >> 2) & 0x01;
-        DATA_CONTROLLER[LeftHatX] = data[2];
-        DATA_CONTROLLER[LeftHatY] = data[3];
-        DATA_CONTROLLER[RightHatX] = data[4];
-        DATA_CONTROLLER[RightHatY] = data[5];
-        DATA_CONTROLLER[L2] = data[6];
-        DATA_CONTROLLER[R2] = data[7];
+        DATA_CONTROLLER[CONNECTION] = (data[0] >> 0) & 0x01;
+        DATA_CONTROLLER[UP] = (data[1] >> 7) & 0x01;
+        DATA_CONTROLLER[RIGHT] = (data[1] >> 6) & 0x01;
+        DATA_CONTROLLER[DOWN] = (data[1] >> 5) & 0x01;
+        DATA_CONTROLLER[LEFT] = (data[1] >> 4) & 0x01;
+        DATA_CONTROLLER[TRIANGLE] = (data[1] >> 3) & 0x01;
+        DATA_CONTROLLER[CIRCLE] = (data[1] >> 2) & 0x01;
+        DATA_CONTROLLER[CROSS] = (data[1] >> 1) & 0x01;
+        DATA_CONTROLLER[SQUARE] = (data[1] >> 0) & 0x01;
+        DATA_CONTROLLER[L1] = (data[2] >> 7) & 0x01;
+        DATA_CONTROLLER[R1] = (data[2] >> 6) & 0x01;
+        DATA_CONTROLLER[SHARE] = (data[2] >> 5) & 0x01;
+        DATA_CONTROLLER[OPTIONS] = (data[2] >> 4) & 0x01;
+        DATA_CONTROLLER[L3] = (data[2] >> 3) & 0x01;
+        DATA_CONTROLLER[R3] = (data[2] >> 2) & 0x01;
+        DATA_CONTROLLER[PS] = (data[2] >> 1) & 0x01;
+        DATA_CONTROLLER[TOUCHPAD] = (data[2] >> 0) & 0x01;
+        DATA_CONTROLLER[LeftHatX] = data[3];
+        DATA_CONTROLLER[LeftHatY] = data[4];
+        DATA_CONTROLLER[RightHatX] = data[5];
+        DATA_CONTROLLER[RightHatY] = data[6];
+        DATA_CONTROLLER[L2] = data[7];
+        DATA_CONTROLLER[R2] = data[8];
+        DATA_TP[X][0] = data[9] >> 8 + data[10];
+        DATA_TP[Y][0] = data[11] >> 8 + data[12];
+        DATA_TP[X][1] = data[13] >> 8 + data[14];
+        DATA_TP[Y][1] = data[15] >> 8 + data[16];
+        
     } else {
-        for(int i = 0; i < 21; i++) {
+        for(int i = 0; i < 23; i++) {
             DATA_CONTROLLER[i] = i >= 15 && i <= 18 ? 127 : 0;
         }
     }
@@ -53,7 +60,7 @@ bool PS4::UpDate()
 
 int PS4::getPress( CONTROLLER buttom )
 {
-    if(buttom < 21) {
+    if(buttom < 23) {
         return DATA_CONTROLLER[buttom];
     } else {
         return 0;
@@ -76,4 +83,9 @@ int PS4::getClick( CONTROLLER button, bool mode )
     } else {
         return 0;
     }
+}
+
+int PS4::touchPad( TP direction, bool finger )
+{
+    return DATA_TP[direction][finger];
 }
--- PS4.h
+++ PS4.h
@@ -5,25 +5,30 @@ enum CONTROLLER {
     UP, RIGHT, DOWN, LEFT,
     TRIANGLE, CIRCLE, CROSS, SQUARE,
     L1, R1,
-    SHARE, OPTIONS,
+    SHARE, OPTIONS,PS,TOUCHPAD,
     L3, R3,
     CONNECTION,
     LeftHatX, LeftHatY,
     RightHatX, RightHatY,
-    L2, R2
+    L2, R2,
+};
+enum TP {
+    X, Y,
 };
 
 class PS4
 {
 private:
-    char data[8];
-    int DATA_CONTROLLER_OLD[21];
+    char data[17];
+    int DATA_CONTROLLER_OLD[23];
     I2C *i2c;
     char ADDR;
 public:
-    int DATA_CONTROLLER[21];
+    int DATA_CONTROLLER[23];
+    int DATA_TP[2][2];
     PS4(I2C *i, char addr);
     bool UpDate();
     int getPress( CONTROLLER buttom );
     int getClick( CONTROLLER button, bool mode );
+    int touchPad( TP direction, bool finger );
 };
